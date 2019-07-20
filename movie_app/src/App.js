26211e47736dbf535fd883c08e8f30b2f42eb7ac
SMK <idsiemu@gmail.com>
2019-07-20 22:51:14 +0900

    Render Update

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -25,10 +25,43 @@ const movies = [
   }
 ]
 
+//컴포넌트는 많은 functions를 가지고 있고 그것들이 순서대로 자동으로 작동한다.
 class App extends Component {
+
+  //Render의 경우 : componentWillMount -- > render -- > componentDidMount
+  //컴포넌트가 존재함과 동시에 3위 3가지 요소가 연속적으로 실행된다.
+
+  //Update componentWillReceiveProps -- > shouldComponentUpdate (기존것과 비교연산하여 다름이 있으면 return true) -- > componentWillUpdate -- > render -- > componentDidUpdate
+  
+
+  componentWillMount(){
+    //사이클이 시작되었음을 알 수 있는 부분
+    console.log('will mount');
+  }
+
+  componentDidMount(){
+    //성공적으로 리액트 세계에 컴포넌트가 자리잡았음을 알게됨
+    console.log('did mount');
+  }
+
+  state = {
+    greeting: 'Hello'
+  }
+
+  componentDidMount(){
+    //setTimeout(() =>{
+      this.setState({
+        greeting: 'something'
+      })
+    //},5000)
+  }
+
   render() {
+    //컴포넌트가 리액트 세계에 존재하게 되었음을 알게됨
+    console.log('did render');
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
         })}
