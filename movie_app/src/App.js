95862744d580b2d65e502d806191ce17156b481b
SMK <idsiemu@gmail.com>
2019-07-22 21:13:13 +0900

    state Adding after rendering

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,28 +2,6 @@ import React, {Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
-  },
-  {
-    title: "SpinderMan",
-    poster: "http://www.globalnewsagency.kr/news/photo/201707/74799_65143_4158.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Star wars",
-    poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
-  }
-]
 
 //컴포넌트는 많은 functions를 가지고 있고 그것들이 순서대로 자동으로 작동한다.
 class App extends Component {
@@ -45,7 +23,29 @@ class App extends Component {
   }
 
   state = {
-    greeting: 'Hello'
+    greeting: 'Hello',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
+      },
+      {
+        title: "SpinderMan",
+        poster: "http://www.globalnewsagency.kr/news/photo/201707/74799_65143_4158.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+      },
+      {
+        title: "Star wars",
+        poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+      }
+    ]
   }
 
   componentDidMount(){
@@ -54,6 +54,20 @@ class App extends Component {
         greeting: 'something'
       })
     //},5000)
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          //this.state.movies,가 없으면 하위에 있는 1개의 영화가 state전체를 대체한다.
+          //응용해서 무한 스크롤 기능을 만들수있다.
+          
+          {
+            title:"인피니터워",
+            poster:"https://w.namu.la/s/c1d766b55e72adfc2c2cad9c5637dc20b64bb32812b69163670a15b505741b5332bf93bcdaa4525871870941c956ea2fb876b8dcd57e2ed99a813cf1f4c631f07258bbd70bc70baca86cbfdeb7569a00d269ce39b50d58cf29b6ec65e3684eca"
+          },
+          ...this.state.movies
+        ]
+      })
+    }, 5000)
   }
 
   render() {
@@ -62,7 +76,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
         })}
       </div>
