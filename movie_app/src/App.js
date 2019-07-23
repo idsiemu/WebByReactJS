e7305c3acc4c547ab458a4a3c91d79e6248933d3
SMK <idsiemu@gmail.com>
2019-07-23 21:05:32 +0900

    state after 5 second componentDidMount

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -23,29 +23,7 @@ class App extends Component {
   }
 
   state = {
-    greeting: 'Hello',
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
-      },
-      {
-        title: "SpinderMan",
-        poster: "http://www.globalnewsagency.kr/news/photo/201707/74799_65143_4158.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-      },
-      {
-        title: "Star wars",
-        poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
-      }
-    ]
+    //greeting: 'Hello',
   }
 
   componentDidMount(){
@@ -56,18 +34,47 @@ class App extends Component {
     //},5000)
     setTimeout(() => {
       this.setState({
-        movies: [
+        //movies: [
           //this.state.movies,가 없으면 하위에 있는 1개의 영화가 state전체를 대체한다.
           //응용해서 무한 스크롤 기능을 만들수있다.
           
+          
+          //...this.state.movies
+        //]
+        movies: [
+          {
+            title: "Matrix",
+            poster: "https://t1.daumcdn.net/movie/e49c2e4eb419a9813228b5ab6bc5b039362236ea"
+          },
+          {
+            title: "SpinderMan",
+            poster: "http://www.globalnewsagency.kr/news/photo/201707/74799_65143_4158.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+          },
+          {
+            title: "Star wars",
+            poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+          },
           {
             title:"인피니터워",
             poster:"https://w.namu.la/s/c1d766b55e72adfc2c2cad9c5637dc20b64bb32812b69163670a15b505741b5332bf93bcdaa4525871870941c956ea2fb876b8dcd57e2ed99a813cf1f4c631f07258bbd70bc70baca86cbfdeb7569a00d269ce39b50d58cf29b6ec65e3684eca"
-          },
-          ...this.state.movies
+          }
         ]
       })
     }, 5000)
+  }
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies
   }
 
   render() {
@@ -75,10 +82,7 @@ class App extends Component {
     console.log('did render');
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
-        })}
+         {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
