01e0317206a170224ec6c6afd4ef2709ee24e56a
SMK <idsiemu@gmail.com>
2019-07-28 21:24:12 +0900

    Updates

---
+++ precache-manifest.f1b646b4bdf597258ddff8e4b7a29f7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434a87136df9f0e539e9aa25f1566369",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f1f2b4538855bd20808c",
+    "url": "/movie_app/static/css/main.09445e83.chunk.css"
+  },
+  {
+    "revision": "542eaa20123cc2a4f8c9",
+    "url": "/movie_app/static/js/2.ad3b0047.chunk.js"
+  },
+  {
+    "revision": "f1f2b4538855bd20808c",
+    "url": "/movie_app/static/js/main.ac6fe899.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
